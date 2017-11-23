8ea271286be673fc4fffed86e60a499f52695c87
ctiller15 <christiller2015@gmail.com>
2017-11-22 20:57:33 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+var app = express();
+
+// For the home page
+app.get("/", (req, res) => {
+	res.send("This is the home page!");
+});
+
+app.get("/Programs", (req, res) => {
+	res.send("This is the Programs page!");
+});
+
+app.get("/Officers", (req, res) => {
+	res.send("This is the Officers page!");
+});
+
+app.get("/Store", (req, res) => {
+	res.send("This is the Store!");
+});
+
+app.get("*", (req, res) => {
+	res.send("Page not found.");
+});
+
+
+app.listen(3000, () => {
+	console.log("started on port 3000!");
+});
